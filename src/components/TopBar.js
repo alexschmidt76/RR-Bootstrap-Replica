adc72a8b04a45490975ea6322ef3b7f409bf8c32
Alex Schmidt <alex.schmidt767@gmail.com>
2022-11-03 20:38:00 -0500

    fixed navbar padding

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,13 +3,13 @@ import logo from '../images/Instacart_logo.png';
 
 export default function TopBar() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="home">
-                <img src={logo} />
+        <Navbar bg="light" expand="lg" style={{padding: '20px 40px 20px 40px' }}>
+            <Navbar.Brand href="home">
+                <img src={logo} style={{ width: '149px '}}/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" />
-            <Nav className="me-auto">
+            <Nav className="me-auto" style={{marginLeft: ""}}>
                 <Nav.Link href="#LogIn">Log In</Nav.Link>
                 <Button variant="success">Sign Up</Button>
             </Nav>
